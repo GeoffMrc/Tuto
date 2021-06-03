d929b6a431729d187f60836f6b9ca364e1d22ec7
gregoryBernard <gregory.bernard.degetel@ext.pmu.fr>
2021-06-03 20:22:07 +0200

    feat(List): add task in tasks state and display

--- tutos/src/components/List/index.js
+++ tutos/src/components/List/index.js
@@ -4,14 +4,17 @@ import { NewTask } from './NewTask';
 const List = () => {
   const [tasks, setTasks] = useState([]);
 
-  const addNewTask = (task) => {
-    console.log(`List: adding new task: ${task}`);
-  };
+  const addNewTask = task => setTasks([...tasks, task]);
 
   return (
     <div>
       List
       <NewTask addNewTask={addNewTask} />
+      {tasks.map((task, index) => (
+        <div key={task + index}>
+          {task}
+        </div>
+      ))}
     </div>
   );
 };
