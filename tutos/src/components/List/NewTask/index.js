437d3022ae8e4721ecda08f5a48b4950501f88ce
gregoryBernard <gregory.bernard.degetel@ext.pmu.fr>
2021-06-03 20:36:45 +0200

    refactor(NewTask): move into separate folder feat(NewTask): update design

--- tutos/src/components/List/NewTask/index.js
+++ tutos/src/components/List/NewTask/index.js
@@ -1,3 +1,4 @@
+import './style.css';
 import { useState } from 'react';
 
 const NewTask = ({ addNewTask }) => {
@@ -12,8 +13,8 @@ const NewTask = ({ addNewTask }) => {
 
   return (
     <>
-      <input type="text" value={text} onChange={e => setText(e.target.value)} placeholder="Task Name" />
-      <button onClick={addTask}>New task</button>
+      <input className="text" type="text" value={text} onChange={e => setText(e.target.value)} placeholder="Task Name" />
+      <button className="button" onClick={addTask}>New task</button>
     </>
   );
 };
